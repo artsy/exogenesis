3190054954d3f9726fe5bc1ec28ccd7b5900231e
Zephraph <zephraph@gmail.com>
2018-06-06 11:45:00 -0400

    prettier, add default display size

--- packages/shared/src/components/elements/Typography.jsx
+++ packages/shared/src/components/elements/Typography.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import styled from "styled-components/primitives";
-import { color, maxWidth, space, textAlign, themeGet } from "styled-system";
+import React from 'react'
+import styled from 'styled-components/primitives'
+import { color, maxWidth, space, textAlign, themeGet } from 'styled-system'
 
-const dynamicTheme = callback => props =>
-  themeGet.apply(null, [].concat(callback(props)))(props);
+const dynamicTheme = (callback) => (props) =>
+  themeGet.apply(null, [].concat(callback(props)))(props)
 
-const selectFontType = ({ weight = "regular", italic }) => {
+const selectFontType = ({ weight = 'regular', italic }) => {
   if (italic) {
-    if (weight === "medium") return "mediumItalic";
-    return "italic";
+    if (weight === 'medium') return 'mediumItalic'
+    return 'italic'
   }
-  return weight;
-};
+  return weight
+}
 
-const fontPath = props => [
+const fontPath = (props) => [
   `fontFamily.${props.family}.${selectFontType(props)}`,
-  "regular" // fallback
-];
+  'regular' // fallback
+]
 
-const fontSize = props => `typeSizes.${props.typeSize}.fontSize`;
+const fontSize = (props) => `typeSizes.${props.typeSize}.fontSize`
 
-const lineHeight = props => `typeSizes.${props.typeSize}.lineHeight`;
+const lineHeight = (props) => `typeSizes.${props.typeSize}.lineHeight`
 
 const StyledText = styled.Text`
   font-family: ${dynamicTheme(fontPath)};
@@ -30,22 +30,22 @@ const StyledText = styled.Text`
   ${space};
   ${textAlign};
   ${maxWidth};
-`;
+`
 
-export const Sans = props => (
+export const Sans = (props) => (
   <StyledText family="unica" typeSize={`sans${props.size || 4}`} {...props} />
-);
-export const Serif = props => (
+)
+export const Serif = (props) => (
   <StyledText
     family="garamond"
     typeSize={`serif${props.size || 4}`}
     {...props}
   />
-);
-export const Display = props => (
+)
+export const Display = (props) => (
   <StyledText
     family="avantgarde"
-    typeSize={`display${props.size}`}
+    typeSize={`display${props.size || 2}`}
     {...props}
   />
-);
+)
